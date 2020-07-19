b4469660091bc993a12bd4f81413d701d9cab0a3
vynguyen-1010 <66165751+vynguyen-1010@users.noreply.github.com>
2020-07-19 09:26:55 +0700

    Hello World

---
+++ Intern-Dev/Intern-Dev/hello_world.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void inHoa(string text) {
+
+	//cin.ignore(); //xóa bộ đệm trước khi nhập chuỗi
+	//cout << "Nhap vao chuoi ky tu can in hoa: ";
+	//getline(cin, text);
+	//cout << "Chuoi ban vua nhap la: " << text;
+
+	for (int i = 0; i < text.size(); i++) {
+		if (text[i] >= 'a' && text[i] <= 'z') {
+			text[i] -= 32;
+		}
+	}
+	cout << "Chuoi ky tu in hoa: " << text << "\n";
+}
+
+int main() {
+
+	cout << "Hello World\n";
+
+	inHoa("nguyen yen vy");
+
+	system("pause");
+	return 0;
+}
